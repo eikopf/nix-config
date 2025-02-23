8ad87aa9ce961058d1b7866adc061ea982fc0f84
Oliver Wooding <oliverwooding@icloud.com>
2025-02-23 15:07:21 +0000

    added configuration for ghc + tooling packages

--- hosts/pilatus/extra-packages.nix
+++ hosts/pilatus/extra-packages.nix
@@ -10,6 +10,7 @@ with pkgs;
   # languages
   chez # chezscheme
   elan # lean
+  ghc # haskell
   go # go
   idris2 # idris2
   janet # janet
@@ -29,4 +30,11 @@ with pkgs;
   idris2Packages.idris2Lsp
   ocamlPackages.utop
   ocamlPackages.ocaml-lsp
+
+  # haskell-tools.nvim prerequisites
+  haskellPackages.fast-tags
+  haskellPackages.haskell-debug-adapter
+  haskellPackages.haskell-language-server
+  haskellPackages.hoogle
+  haskellPackages.stack
 ]
