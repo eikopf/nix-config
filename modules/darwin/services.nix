f70c61d8d4561d64ae243deb705de1709d217f3e
Oliver Wooding <oliverwooding@icloud.com>
2025-03-16 13:47:14 +0000

    updated skhd + yabai configs in modules/darwin

--- modules/darwin/services.nix
+++ modules/darwin/services.nix
@@ -12,8 +12,40 @@
     enable = true;
 
     skhdConfig = lib.strings.concatLines [
-      "cmd - return: open -a /Applications/Ghostty.app"
-      "cmd + shift - return: neovide"
+      # shortcuts to open ghostty and neovide
+      "cmd - return : open -a /Applications/Ghostty.app"
+      "cmd + shift - return : neovide"
+
+      # move focus between windows in the current display
+      "alt - h : yabai -m window --focus west"
+      "alt - j : yabai -m window --focus south"
+      "alt - k : yabai -m window --focus north"
+      "alt - l : yabai -m window --focus east"
+
+      # swap windows directionally
+      "shift + alt - h : yabai -m window --swap west"
+      "shift + alt - j : yabai -m window --swap south"
+      "shift + alt - k : yabai -m window --swap north"
+      "shift + alt - l : yabai -m window --swap east"
+
+      # move windows directionally
+      "ctrl + alt - h : yabai -m window --warp west"
+      "ctrl + alt - j : yabai -m window --warp south"
+      "ctrl + alt - k : yabai -m window --warp north"
+      "ctrl + alt - l : yabai -m window --warp east"
+
+      # rotate tree
+      "alt - r : yabai -m space --rotate 90"
+
+      # mirror tree vertically
+      "alt - y : yabai -m space --mirror y-axis"
+
+      # toggle window padding and gap in current space
+      "alt - a : yabai -m space --toggle padding; yabai -m space --toggle gap"
+
+      # change layout in current space
+      "ctrl + alt - a : yabai -m space --layout bsp" # (activate)
+      "ctrl + alt - d : yabai -m space --layout float" # (deactivate)
     ];
   };
 
@@ -38,8 +70,13 @@
         left_padding = padding;
         right_padding = padding;
         window_gap = padding;
+        window_placement = "second_child";
       };
 
-    extraConfig = "yabai -m rule --add app='^System Settings$' manage=off";
+    extraConfig = lib.strings.concatLines [
+      # ignored apps
+      "yabai -m rule --add app='^System Settings$' manage=off"
+      "yabai -m rule --add app='^Archive Utility$' manage=off"
+    ];
   };
 }
