8ec8388582d74fb5373d426823c0844c42da3440
Oliver Wooding <oliverwooding@icloud.com>
2025-12-30 13:13:30 +0100

    bumped channel to 25.11

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "Core Nix configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
+      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -53,7 +53,8 @@
               home-manager.backupFileExtension = "backup";
               home-manager.users.oliver = (import ./home);
             }
-          ] ++ extraModules;
+          ]
+          ++ extraModules;
         };
 
       mkDarwinHost =
@@ -81,7 +82,8 @@
               home-manager.backupFileExtension = "backup";
               home-manager.users.oliver = import ./home;
             }
-          ] ++ extraModules;
+          ]
+          ++ extraModules;
         };
     in
     {
