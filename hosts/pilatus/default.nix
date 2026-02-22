914871b222a52847acfcd92860882e89a0934f8b
Oliver Wooding <oliverwooding@icloud.com>
2026-02-22 19:53:34 +0100

    pilatus: added pnpm_9 to system packages

--- hosts/pilatus/default.nix
+++ hosts/pilatus/default.nix
@@ -54,6 +54,7 @@
 
     gh # github CLI integration
     tmux # terminal multiplexer
+    pnpm_9 # javascript package manager
 
     # macos utilities
     monitorcontrol # external display controller
