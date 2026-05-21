26bf65e0962769d7ea3098a7408335fe9c974bc2
Oliver Wooding <oliverwooding@icloud.com>
2026-05-21 07:57:34 +0200

    nixos: adjusted locale settings

--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -3,13 +3,11 @@
   time.timeZone = "Europe/Zurich";
 
   i18n.defaultLocale = "en_US.UTF-8";
+
+  # see locale(5) and locale(7) for precise definitions
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_CH.UTF-8";
-    LC_IDENTIFICATION = "de_CH.UTF-8";
     LC_MEASUREMENT = "de_CH.UTF-8";
     LC_MONETARY = "de_CH.UTF-8";
-    LC_NAME = "de_CH.UTF-8";
-    LC_NUMERIC = "de_CH.UTF-8";
     LC_PAPER = "de_CH.UTF-8";
     LC_TELEPHONE = "de_CH.UTF-8";
     LC_TIME = "de_CH.UTF-8";
