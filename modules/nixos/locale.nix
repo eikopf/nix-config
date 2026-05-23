afd1cb16e851357c3822caf310046f4252554b61
oliver <oliver@wooding.dev>
2026-05-23 10:45:25 +0200

    nixos: modified locale settings

--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -6,10 +6,10 @@
 
   # see locale(5) and locale(7) for precise definitions
   i18n.extraLocaleSettings = {
-    LC_MEASUREMENT = "de_CH.UTF-8";
+    LC_MEASUREMENT = "en_DK.UTF-8";
     LC_MONETARY = "de_CH.UTF-8";
     LC_PAPER = "de_CH.UTF-8";
     LC_TELEPHONE = "de_CH.UTF-8";
-    LC_TIME = "de_CH.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
   };
 }
