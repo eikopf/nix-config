541692709e7f3de0483d1d24e747ef325d219768
oliver <oliver@wooding.dev>
2026-06-13 14:21:52 +0200

    home: enabled anki-connect and review-heatmap addons for anki

--- home/anki.nix
+++ home/anki.nix
@@ -1,6 +1,11 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   programs.anki = {
     enable = lib.mkDefault false;
+
+    addons = with pkgs.ankiAddons; [
+      anki-connect
+      review-heatmap
+    ];
   };
 }
