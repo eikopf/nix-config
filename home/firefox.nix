7e0d2d78b156c6166a0bab18f2ac7e75fff6d0ed
oliver <oliver@wooding.dev>
2026-06-20 13:00:24 +0200

    home: corrected firefox extensions and disabled first-run/post-update pages

--- home/firefox.nix
+++ home/firefox.nix
@@ -11,6 +11,8 @@
       DontCheckDefaultBrowser = true;
       OfferToSaveLoginsDefault = false;
       SkipTermsOfUser = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
 
       # fix download directory to match $XDG_DOWNLOAD_DIR
       DefaultDownloadDirectory = config.xdg.userDirs.download;
@@ -31,14 +33,17 @@
 
       ExtensionSettings = {
         # uBlock Origin
-        "uBlock0@raymondhil.net" = {
-          installation_mode = "force_installed";
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "normal_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          private_browsing = true;
         };
         # Bitwarden
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          installation_mode = "force_installed";
+          installation_mode = "normal_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          private_browsing = true;
+          default_area = "navbar";
         };
       };
     };
